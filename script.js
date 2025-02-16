9687139e6c17347011dfb742bed7f190c28bf962
Tomas Gonzalez Humphreys <tomasgonzalezh@MacBookAir.fibertel.com.ar>
2025-02-16 01:58:19 -0300

    html and css in process. The squeme of a calculator is done, but not functional yet.

---
+++ script.js
@@ -0,0 +1,4 @@
+// SI toco igual, muestra resultado en la variable cuenta
+// si toco algun boton de operacion, lo suma a la cuenta y despues lo muestra en el display de los calculos remaining
+// C de clear solo saca un digito de la cuenta
+// AC de ALL CLEAR elimina la cuenta por completo
