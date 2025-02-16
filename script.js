4950ecf58a4264172fbf2d11484612cffcd4bad1
Tomas Gonzalez Humphreys <tomasgonzalezh@MacBookAir.fibertel.com.ar>
2025-02-16 16:46:13 -0300

    Added a color pallete, improved style. No functionality yet

--- script.js
+++ script.js
@@ -2,3 +2,4 @@
 // si toco algun boton de operacion, lo suma a la cuenta y despues lo muestra en el display de los calculos remaining
 // C de clear solo saca un digito de la cuenta
 // AC de ALL CLEAR elimina la cuenta por completo
+
